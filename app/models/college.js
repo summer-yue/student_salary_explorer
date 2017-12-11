2fba6896afcc707aedcdee4699dc98c4dfcc213b
Summer Yue <yutingyue514@gmail.com>
2017-12-10 22:56:10 -0500

    Simplify code by moving api.js and model.js into server.js since it's a simple app

--- app/models/college.js
+++ app/models/college.js
@@ -1,22 +1,22 @@
-var mongoose = require('mongoose'); // Import Mongoose Package
-var Schema = mongoose.Schema; // Assign Mongoose Schema function to variable
-var bcrypt = require('bcrypt-nodejs'); // Import Bcrypt Package
-var titlize = require('mongoose-title-case'); // Import Mongoose Title Case Plugin
-var validate = require('mongoose-validator'); // Import Mongoose Validator Plugin
+// var mongoose = require('mongoose'); // Import Mongoose Package
+// var Schema = mongoose.Schema; // Assign Mongoose Schema function to variable
+// var bcrypt = require('bcrypt-nodejs'); // Import Bcrypt Package
+// var titlize = require('mongoose-title-case'); // Import Mongoose Title Case Plugin
+// var validate = require('mongoose-validator'); // Import Mongoose Validator Plugin
 
-// College Mongoose Schema
-var CollegeSchema = mongoose.Schema({
-    "_id": { type: String, required: true},
-    "avg_net_price_private": { type: String },
-    "out_of_state_tuition": { type: String },
-    "in_state_tuition": { type: String },
-    // avg_net_price_public: { type: String },
-    // school_name: { type: String },
-    // admissions_rate: { type: String },
-    // state: { type: String },
-    // city: { type: String },
-    // student_number: { type: String },
-    // school_url: { type: String },
-});
+// // College Mongoose Schema
+// var CollegeSchema = mongoose.Schema({
+//     "_id": { type: String, required: true},
+//     "avg_net_price_private": { type: String },
+//     "out_of_state_tuition": { type: String },
+//     "in_state_tuition": { type: String },
+//     // avg_net_price_public: { type: String },
+//     // school_name: { type: String },
+//     // admissions_rate: { type: String },
+//     // state: { type: String },
+//     // city: { type: String },
+//     // student_number: { type: String },
+//     // school_url: { type: String },
+// });
 
-module.exports = mongoose.model('college_score_card', CollegeSchema); // Export College Model for us in API
+// module.exports = mongoose.model('college_score_card', CollegeSchema); // Export College Model for us in API
