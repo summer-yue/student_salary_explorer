9c2c22d75f1fee8df2070efce3bbea0a00e7c5fb
Summer Yue <yutingyue514@gmail.com>
2017-12-11 21:06:58 -0500

    By salary page skeleton, need to connect to SQL

--- public/app/controllers/bySalaryCtrl.js
+++ public/app/controllers/bySalaryCtrl.js
@@ -5,11 +5,19 @@ angular.module('bySalaryController', []) /*injecting services used*/
 .controller('bySalaryCtrl', function($scope, $http) {
     var app = this;
     app.loadme = false; // Hide main HTML until data is obtained in AngularJS
-
+    $scope.colleges_in_range = "Please select range and click search";
+    $scope.majors_in_range = "Please select range and click search";
     //console.log("Entering by salary controller api");
     
-    // $http.get('/api/college_info/temp').success(function(result){
-    //     $scope.current_college_info = result.data;
-    //     // console.log(result);
-    // });
+    $scope.display_college_major_in_range = function() {
+        $http.get('/api/colleges_based_on_salary_range/1/20').success(function(result){
+            $scope.colleges_in_range = result.data;
+        });
+
+        $http.get('/api/majors_based_on_salary_range/1/20').success(function(result){
+            $scope.majors_in_range = result.data;
+        });
+
+        console.log("Salary ranging from " + $scope.salary_range_low + " to " + $scope.salary_range_high);
+    }
 });
