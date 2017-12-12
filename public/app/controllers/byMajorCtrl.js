becafd9ea51389f385bbe47d520c3756d97c3f67
Summer Yue <yutingyue514@gmail.com>
2017-12-12 14:53:05 -0500

    Feature 1b

--- public/app/controllers/byMajorCtrl.js
+++ public/app/controllers/byMajorCtrl.js
@@ -53,7 +53,14 @@ angular.module('byMajorController', []) /*injecting services used*/
                 })
             })
         })
-        setTimeout(display_salary_bar_chart, 1000); //Allowing time to load data
+
+        display_all_charts = function(){
+            display_salary_bar_chart();
+            //display_gender_pie_chart();
+            display_employment_bar_chart();
+        }
+
+        setTimeout(display_all_charts, 1000); //Allowing time to load data
         //TODO: make this a callback
     }
 
@@ -112,5 +119,62 @@ angular.module('byMajorController', []) /*injecting services used*/
             .attr("width", xScale.rangeBand())
             .attr("y", function(d) { return yScale(d.median_salary); })
             .attr("height", function(d) { return height - yScale(d.median_salary); });
+    }
+
+    display_employment_bar_chart = function() {
+        var margin = {top:100, right:100, bottom:100, left:100};
+
+        var width = 960 - margin.left - margin.right;
+
+        var height = 500 - margin.top - margin.bottom;
+
+        var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .5)
+
+        var yScale = d3.scale.linear()
+              .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+                .scale(xScale)
+                .orient("bottom");
+              
+        var yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient("left");
+
+        var svgContainer = d3.select("#employment-bar-chart").append("svg")
+                .attr("width", width+margin.left + margin.right)
+                .attr("height",height+margin.top + margin.bottom)
+                .append("g").attr("class", "container")
+                .attr("transform", "translate("+ margin.left +","+ margin.top +")");
+
+        xScale.domain($scope.unemployment_rates.map(function(d) { return utilService.firstWord(d.major_name); }));
+        yScale.domain([d3.min($scope.unemployment_rates, function(d) { return d.unemployment_rate; }) - 0.01,
+            d3.max($scope.unemployment_rates, function(d) { return d.unemployment_rate; }) + 0.01]);
+
+        //xAxis. To put on the top, swap "(height)" with "-5" in the translate() statement. Then you'll have to change the margins above and the x,y attributes in the svgContainer.select('.x.axis') statement inside resize() below.
+        var xAxis_g = svgContainer.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + (height) + ")")
+                .call(xAxis)
+                .selectAll("text");
+                    
+        // Uncomment this block if you want the y axis
+        var yAxis_g = svgContainer.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6).attr("dy", ".71em")
+                //.style("text-anchor", "end").text("Number of Applicatons"); 
+
+        svgContainer.selectAll(".bar")
+            .data($scope.unemployment_rates)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return xScale(utilService.firstWord(d.major_name)); })
+            .attr("width", xScale.rangeBand())
+            .attr("y", function(d) { return yScale(d.unemployment_rate); })
+            .attr("height", function(d) { return height - yScale(d.unemployment_rate); });
     }
 });
